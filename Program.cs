a5dd2e9dca278ebbec736b758e69bd754bd6a0a5
Daria2945 <148208175+Daria2945@users.noreply.github.com>
2025-07-02 14:34:12 +0300

    Update 1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 ﻿public static int SearchElementIndex(int[] array, int element)
 {
+    if(array == null)
+        throw new ArgumentNullException(nameof(array));
+
+    if (array.Length == 0)
+        throw new ArgumentException($"{nameof(array)} не был заполнен");
+
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i] == element)
